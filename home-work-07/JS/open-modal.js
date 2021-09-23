78e2006a7ec94822ade98ea29178df56153426f9
Pavel Omelchenko <p.omel4enko2002@gmail.com>
2021-09-23 19:10:06 +0300

    Added home-work 07

---
+++ home-work-07/JS/open-modal.js
@@ -0,0 +1,18 @@
+const refs = {
+	openModalBtn: document.querySelector('[data-open-modal]'),
+	closeModalBtn: document.querySelector('[data-close-modal]'),
+	backdrop: document.querySelector('[data-backdrop]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+refs.backdrop.addEventListener('click', LogBackdropClick);
+
+function toggleModal() {
+	refs.backdrop.classList.toggle('is-hidden');
+}
+
+function LogBackdropClick() {
+	console.log('You clicked into the backdrop!!!');
+}
